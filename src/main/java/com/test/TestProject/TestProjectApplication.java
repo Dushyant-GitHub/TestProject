c6066b096d2a8d9eea9e5b418a115fec9e6c570e
Poonam S <prachiswaghmode@gmail.com>
2022-10-29 20:54:43 +0530

    multiply added

--- src/main/java/com/test/TestProject/TestProjectApplication.java
+++ src/main/java/com/test/TestProject/TestProjectApplication.java
@@ -16,4 +16,10 @@ public class TestProjectApplication {
 		int result = a+b;
 		System.out.println("Addition is : "+result);
 	}
+
+	private static void multiplyNumber(int a, int b){
+		int result = a*b;
+		System.out.println("Multiplication is : "+result);
+	}
+
 }
