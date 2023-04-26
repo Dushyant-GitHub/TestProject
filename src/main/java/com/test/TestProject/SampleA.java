6997360b3f0d8294155178880ff1618dd52c096e
Poonam S <prachiswaghmode@gmail.com>
2023-04-26 16:57:00 +0530

    testing rebase

--- src/main/java/com/test/TestProject/SampleA.java
+++ src/main/java/com/test/TestProject/SampleA.java
@@ -18,8 +18,14 @@ public class SampleA {
         System.out.println("Multiplication ....");
         return a*b;
     }
+<<<<<<< Updated upstream
     
      public void greetingTheWorld(){
          System.out.println("Hello World.....By Dushyant");
+=======
+
+    public void sayOlaToTheWorld(){
+        System.out.println("say OLAAAAAAA !!!!!");
+>>>>>>> Stashed changes
     }
 }
