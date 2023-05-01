82906a3bf5973b6eb79aca27e955bd61732d4155
Poonam S <prachiswaghmode@gmail.com>
2023-05-01 17:11:32 +0530

    added helloWorld class

---
+++ src/main/java/com/test/TestProject/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.test.TestProject;
+
+public class HelloWorld {
+
+    public void sayToWorld(){
+        System.out.println("Hello World ..");
+        System.out.println("Vasudeiva kutumbakum..!!");
+    }
+}
